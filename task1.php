e64bf81ee113231eb9cb047bf65ca1d83808a1e8
Umairali2562 <umairali2562@gmail.com>
2022-04-01 15:02:33 +0500

    start of task 1

---
+++ task1.php
@@ -0,0 +1,14 @@
+/*Task 1
+
+Create a character counter.
+Given an input, produce an output that gives out the character position individually.
+Example:
+
+Input: Sample
+
+Output: S-1, a-2, m-3, p-4, l-5, e-6
+
+Input: Test
+
+Output: T-1, e-2, s-3, t-4/
+
